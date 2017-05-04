5392b9be3f58230a7ab8ace18a94d6d75a35c263
Gibran Wisesa <gibranmfw@gmail.com>
2017-05-04 18:58:35 +0700

    write new module called meme (#34)

---
+++ csuibot/utils/meme.py
@@ -0,0 +1,38 @@
+class MemeGenerator:
+
+	memelist = ['61579']
+
+	def __init__():
+		raise NotImplementedError
+
+	def createID():
+		raise NotImplementedError
+
+	def createBottomCaption():
+		raise NotImplementedError
+
+	def createTopCaption():
+		raise NotImplementedError
+
+	def generateMeme():
+		raise NotImplementedError
+
+class Meme:
+	_id = ""
+	_bottomCaption = ""
+	_topCaption = ""
+
+	def __init__():
+		raise NotImplementedError
+
+	def geyID():
+		raise NotImplementedError
+
+	def getBottomCaption():
+		raise NotImplementedError
+
+	def getTopCaption():
+		raise NotImplementedError
+
+	def set():
+		raise NotImplementedError
