9acf578bf2301c8384f325d5635311d60a36ab6e
bagas prasetya adi <bagasprasetya1702@gmail.com>
2017-05-05 03:09:13 +0700

    finished All, coverage 100%

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,6 +1,6 @@
 from unittest.mock import Mock
 
-from csuibot.handlers import help, zodiac, shio
+from csuibot.handlers import help, zodiac, shio, compute
 
 
 def test_help(mocker):
@@ -61,3 +61,51 @@ def test_shio_invalid_year(mocker):
 
     args, _ = mocked_reply_to.call_args
     assert args[1] == 'Year is invalid'
+
+def test_compute_binary_valid_addtion(mocker) :
+    mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
+    mocker.patch('csuibot.handlers.calculate_binary', return_value = '7')
+    mock_message = Mock(text='/compute 0011+0100')
+
+    compute(mock_message)
+
+    args, _ = mocked_reply_to.call_args
+    assert args[1] == '7'
+def test_compute_binary_valid_subtraction(mocker) :
+    mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
+    mocker.patch('csuibot.handlers.calculate_binary', return_value = '4')
+    mock_message = Mock(text='/compute 1000-0100')
+
+    compute(mock_message)
+
+    args, _ = mocked_reply_to.call_args
+    assert args[1] == '4'
+def test_compute_binary_valid_multiplication(mocker) :
+    mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
+    mocker.patch('csuibot.handlers.calculate_binary', return_value = '12')
+    mock_message = Mock(text='/compute 0011*0100')
+
+    compute(mock_message)
+
+    args, _ = mocked_reply_to.call_args
+    assert args[1] == '12'
+def test_compute_binary_valid_division(mocker) :
+    mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
+    mocker.patch('csuibot.handlers.calculate_binary', return_value = '2')
+    mock_message = Mock(text='/compute 1000/0100')
+
+    compute(mock_message)
+
+    args, _ = mocked_reply_to.call_args
+    assert args[1] == '2'
+    
+def test_compute_invalid_input(mocker) :
+    mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
+    mocker.patch('csuibot.handlers.calculate_binary', side_effect=ValueError)
+    mock_message = Mock(text='/compute 1234+12311')
+
+    compute(mock_message)
+
+    args, _ = mocked_reply_to.call_args
+    assert args[1] == 'Not a binary number or operator is invalid!'
+    
