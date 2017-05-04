5081eedef613f94381cd3c730a10cd9290326217
Gibran Wisesa <gibranmfw@gmail.com>
2017-05-04 19:31:26 +0700

    create new modul called hipster.py

---
+++ csuibot/utils/hipster.py
@@ -0,0 +1,19 @@
+class HipsterGenerator:
+
+	def __init__():
+		raise NotImplementedError
+
+	def generateHipster():
+		raise NotImplementedError
+
+class HipsterIpsum:
+	_paras = ""
+
+	def __init__():
+		raise NotImplementedError
+
+	def getParagragh():
+		raise NotImplementedError
+
+	def set():
+		raise NotImplementedError
