2ef24b5144f1b9a4679ce3f829d90c2125f58514
Nur Intan Alatas <Nur Intan Alatas>
2017-05-04 22:20:47 +0700

    create kelaskata.py base class and stub (#27)

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -44,3 +44,7 @@ def shio(message):
 
 def parse_date(text):
     return tuple(map(int, text.split('-')))
+
+@bot.message_handler(regexp=r'^/kelaskata (.*)$')
+def kelaskata(message):
+    pass
---
+++ csuibot/utils/kelaskata.py
@@ -0,0 +1,6 @@
+class KelasKata:
+	def __init__(self):
+		pass
+
+	def getKelasKata(self):
+		pass
