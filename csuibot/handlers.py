de25dda3235bdecd303422953164b3b63374f115
rinaldi <rinaldiandrian@gmail.com>
2017-05-04 15:22:17 +0700

    Add function stubs (#24)

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -42,5 +42,10 @@ def shio(message):
         bot.reply_to(message, zodiac)
 
 
+@bot.message_handler(regexp=r'^/bot ip$')
+def ip(message):
+    pass
+
+
 def parse_date(text):
     return tuple(map(int, text.split('-')))
