f4eb405b7fb1fab2d078386eac7ff085b4eb4492
nurintaaan <intannnalatas@gmail.com>
2017-05-06 07:13:21 +0700

    add everything

--- csuibot/config.py
+++ csuibot/config.py
@@ -5,4 +5,4 @@ APP_ENV = environ.get('APP_ENV', 'development')
 DEBUG = environ.get('DEBUG', 'true') == 'true'
 TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', '335489012:AAF4h9QaE0TmnKK6CC8t0TFnbhI2TW-g6r8')
 LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG')
-WEBHOOK_HOST = environ.get('WEBHOOK_HOST', 'https://powerful-falls-55212.herokuapp.com/bot')
+WEBHOOK_HOST = environ.get('WEBHOOK_HOST', 'https://powerful-falls-55212.herokuapp.com')
