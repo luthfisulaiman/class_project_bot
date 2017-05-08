69b3869c16a83bd6a15c5bba2ade9992ae0cb133
muhammad.faiz <saravedian@gmail.com>
2017-05-08 12:17:16 +0700

    fixing link to detailed version of the post

--- csuibot/utils/notifTaker.py
+++ csuibot/utils/notifTaker.py
@@ -6,28 +6,41 @@ class notifTaker:
     def __init__(self):
         self.post = "notif"
 
-    def makeNotifReport(self, subject, info, post):
+    def makeNotifReport(self, subject, info, post, detail):
         laporan = ""
         counter = 0
         subLaporan = ""
         for judul in subject:
             subLaporan = subject[counter].contents[0].upper() + info[counter] + "\n"
             subLaporan += post[counter]
-            subLaporan += "\n \n scele.cs.ui.ac.id \n =================================== \n"
+            subLaporan += "\n \n "+detail[counter] +"\n =================================== \n"
             laporan += subLaporan
             counter += 1
         return laporan
 
     def getPost(self):
         r = urllib.request.urlopen("https://scele.cs.ui.ac.id/").read()
+        detailed = urllib.request.urlopen("https://scele.cs.ui.ac.id/mod/forum/view.php?f=1").read()
         soup = BeautifulSoup(r, "html.parser")
+        detailedSoup = BeautifulSoup(detailed,"html.parser")
         subject = soup.find_all("div", class_="subject")
         info = soup.find_all("div", class_="author")
         post = soup.find_all("div", class_="posting")
         flagNavString = type(post[1].contents[0].contents[0])
+        detailJadi = self.olahDetail(detailedSoup)
         infoJadi = self.olahInfo(info)
         postJadi = self.olahPost(post, flagNavString)
-        return self.makeNotifReport(subject, infoJadi, postJadi)
+        print(self.makeNotifReport(subject, infoJadi, postJadi, detailJadi))
+
+    def olahDetail(self,soup):
+        outerContainer = soup.select('td.topic.starter')[:5]
+        # only getting the first 5 (or according to scele front page)
+        arrayHasil =[]
+        for element in outerContainer:
+            hasil = ""
+            hasil = element.a["href"] 
+            arrayHasil.append(hasil)
+        return arrayHasil
 
     def olahInfo(self, arrayInfo):
         arrayHasil = []
