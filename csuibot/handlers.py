5bf1651f423eb73ad4354c4c1e9bf00a3eb0fad9
bagas prasetya adi <bagasprasetya1702@gmail.com>
2017-05-09 09:04:20 +0700

    change from min to second

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -59,7 +59,7 @@ def remind(message):
             text += time_str[i] + " "
         i += 1
     try:
-        bot.reply_to(message, "You have a new reminder in " + time_str[1] + " minutes")
+        bot.reply_to(message, "You have a new reminder in " + time_str[1] + " second")
         app.logger.debug(time_str[1])
         reply_text = remind_me(time_str[1], text)
         app.logger.debug(time_str[1])
