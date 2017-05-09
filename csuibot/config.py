33b063aff3abfc9fa0d9466e52c5f24911465c77
Qurrata Ayuna Adrianus <yunadrianus@gmail.com>
2017-05-09 13:05:21 +0700

    edit

--- csuibot/config.py
+++ csuibot/config.py
@@ -1,8 +1,9 @@
 from os import environ
 
 
+mytoken = '374007525:AAEP0C78JLnn6oq-wK2xs2LfY0rsavW9j88'
 APP_ENV = environ.get('APP_ENV', 'development')
 DEBUG = environ.get('DEBUG', 'true') == 'true'
-TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', '374007525:AAEP0C78JLnn6oq-wK2xs2LfY0rsavW9j88')
+TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', mytoken)
 LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG')
 WEBHOOK_HOST = environ.get('WEBHOOK_HOST', 'https://newicktreeapp.herokuapps.com')
