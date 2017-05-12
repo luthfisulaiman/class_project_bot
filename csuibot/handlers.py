d621a686e1d0c0548ea4dd4091fd2152278e99d6
stephenjaya99 <stephenjaya99@gmail.com>
2017-05-12 17:07:00 +0700

    added youtube handlers

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -521,3 +521,8 @@ def marsfasilkom(message):
         bot.reply_to(message, 'Command /marsfasilkom doesn\'t need any arguments')
     else:
         bot.reply_to(message, marsfasilkom)
+
+
+@bot.message_handler(regexp=r'^(\/youtube) .+$')
+def youtube(url):
+    pass
