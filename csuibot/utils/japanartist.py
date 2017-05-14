9c4f1e6580635722db2bf69355223f6f5c8c71e9
stephenjaya99 <stephenjaya99@gmail.com>
2017-05-14 22:52:24 +0700

    edit messages output

--- csuibot/utils/japanartist.py
+++ csuibot/utils/japanartist.py
@@ -12,9 +12,9 @@ class JapanArtist:
         soup = bs(charts, 'html.parser')
         for chart in soup.find_all('item'):
             if(artist in chart.artist.string):
-                return "🎤 Artist 🎤   : {} \n \
-                        🎶 Song 🎶     : {} \n \
-                        📈 Position 📈 : {} \n "\
+                return "🎤 Artist 🎤  :{} \n\
+                        🎶 Song 🎶    :{} \n \
+                        📈 Position 📈:{} \n"\
                         .format(chart.artist.string,
                                 chart.chart_item_title.string,
                                 chart.rank_this_week.string)
