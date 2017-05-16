57ff2839c3e915a6e49c3eec65b2c4eca9412911
Wildaspt <wildaspt@gmail.com>
2017-05-17 00:41:05 +0700

    fix test_handlers.py

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -80,10 +80,10 @@ def test_shio_invalid_year(mocker):
 
 
 def test_sentiment(mocker):
-    fake_reply = 'This word is :\nPositive : 0.5\nNegative : 0.5'
+    fake_reply = 'Positive: 0.5\nNegative: 0.5'
     mocked_reply_to = mocker.patch('csuibot.handlers.bot.reply_to')
     mocker.patch('csuibot.handlers.lookup_sentiment', return_value=fake_reply)
-    mock_message = Mock(text='/sentiment good nice bad terrible movie sound')
+    mock_message = Mock(text='/sentiment good nice bad terrible')
 
     sentiment(mock_message)
     args, _ = mocked_reply_to.call_args
