d416b0cd52f7ac521e4931278c3842525b457a4e
muhammad.faiz <saravedian@gmail.com>
2017-05-19 14:56:50 +0700

    pembuatan stub class di util diceSim

---
+++ csuibot/utils/diceSim.py
@@ -0,0 +1,17 @@
+class diceSim:
+
+    def __init__:
+        self.post = "dice"
+
+    def coin:
+        pass
+
+    def roll:
+        pass
+
+    def multiroll:
+        pass
+
+    def is_lucky:
+        pass
+    
