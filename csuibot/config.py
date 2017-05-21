fe0e91af0e45473f51a1b83945d3e868f8b08fa1
Gunawan <gunawansantoso.lmr@gmail.com>
2017-05-21 18:57:48 +0700

    Remove unused text.

--- csuibot/config.py
+++ csuibot/config.py
@@ -3,7 +3,7 @@ from os import environ
 
 APP_ENV = environ.get('APP_ENV', 'development')
 DEBUG = environ.get('DEBUG', 'true') == 'true'
-TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', '353233731:AAErSCOhfoyv-QxLOHofNp9iZnOwgvAGhJ8')
+TELEGRAM_BOT_TOKEN = environ.get('TELEGRAM_BOT_TOKEN', 'randomstring')
 LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG')
 WEBHOOK_HOST = environ.get('WEBHOOK_HOST', 'https://csui-bot-lightmire.herokuapp.com/')
 IMAGGA_API_KEY = environ.get('IMAGGA_API_KEY', 'somerandomstring')
