b19e13c0573aecfabea10b9011be35b70e80cb59
stephenjaya99 <stephenjaya99@gmail.com>
2017-05-21 16:31:48 +0700

    implemented add destination

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -1048,8 +1048,8 @@ def process_location_step(message):
     app.logger.debug('location step detected')
     app.logger.debug(message.location)
     try:
-        lon = message.location.longitude
-        lat = message.location.latitude
+        lon = message.location['longitude']
+        lat = message.location.['latitude']
         app.logger('{}{}'.format(lon, lat))
         loc = Location(lat, lon)
         locations[message.chat.id] = loc
