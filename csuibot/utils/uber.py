5b67767be6c8e308eb2e54a5835e44e25a77bd60
stephenjaya99 <stephenjaya99@gmail.com>
2017-05-21 22:36:05 +0700

    implemented add destination

--- csuibot/utils/uber.py
+++ csuibot/utils/uber.py
@@ -1,9 +1,42 @@
+import json
+import io
+from os import path
+
 class Uber:
-	def __init__(self):
-		pass
+    class __Uber:
+        def __init__(self):
+            self.locations_json_path = \
+            path.abspath(path.join(path.dirname(__file__), "locations.json"))
+            self.importJSON()
 
-	def add_destination(self):
-		pass
+        def importJSON(self):
+            try:
+                with open(self.locations_json_path, 'r') as locations_json:
+                    self.locations_dict = json.load(locations_json)
+            except IOError:
+                self.locations_dict = {}
+                self.locations_dict['locations'] = {}
+                with open(self.locations_json_path, 'w') as locations_json:
+                    json.dump(self.locations_dict, locations_json)
 
-	def remove_destination(self):
-		pass
+        def exportJSON(self):
+            with open(self.locations_json_path, 'r') as locations_json:
+                json.dump(self.locations_dict, locations_json)
+
+        def get_locations(self):
+            return self.locations_dict.keys()
+
+        def add_destination(self, location):
+            self.locations_dict[location.name] = {}
+            self.locations_dict[location.name]['latitude'] = location.lat
+            self.locations_dict[location.name]['longitude'] = location.lon     
+            exportJSON()       
+
+        def remove_destination(self, location_name):
+            self.locations_dict.pop(location_name, None)
+
+    instance = None
+    def __new__(cls):
+        if not Uber.instance:
+            Uber.instance = Uber.__Uber()
+        return Uber.instance
