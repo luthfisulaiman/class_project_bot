6ab971212b8cb84ac2aa47e9b1c69f63ed5b0fbd
nurintaaan <intannnalatas@gmail.com>
2017-05-22 19:51:14 +0700

    bugs

--- csuibot/utils/quran.py
+++ csuibot/utils/quran.py
@@ -1,6 +1,12 @@
+import json
+
 class quran:
     def __init__(self):
-        pass
+    	file = open('csuibot/utils/quran.json', 'r')
+    	self.quran = json.load(file)
+    	file = open('csuibot/utils/quran-translation.json', 'r')
+    	self.quran-translation = json.load(file)
 
-    def lookup_Quran(self):
-        pass
+    def lookup_Quran(self, chapter, verse):
+    	pass
+
