ad2b79e9e2bcdbd66c742b8e2c8a9d4e18d42114
stephenjaya99 <stephenjaya99@gmail.com>
2017-05-23 13:41:21 +0700

    revised handlers

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -1039,7 +1039,7 @@ def add_destination(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton('Share Location', request_location=True)
     markup.row(button)
-    msg = bot.send_message(message.chat.id, "Please share your location", reply_markup=markup)
+    msg = bot.reply_to(message, "Please share your location", reply_markup=markup)
     bot.register_next_step_handler(msg, process_location_step)
 
 
