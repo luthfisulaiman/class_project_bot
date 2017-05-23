69db493d4732cacec6ef708deda603dba1a137ed
Wildaspt <wildaspt@gmail.com>
2017-05-23 17:39:54 +0700

    implementing template method

--- csuibot/utils/__init__.py
+++ csuibot/utils/__init__.py
@@ -7,7 +7,6 @@ import urllib.error
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
-from datetime import datetime
 from csuibot.utils import (zodiac as z, ip, palindrome as p, hipster as hp,
                            loremipsum as li, hex2rgb as h, xkcd as x, meme,
                            password as pw, custom_chuck as cc, kelaskata as k,
@@ -25,7 +24,7 @@ from csuibot.utils import (zodiac as z, ip, palindrome as p, hipster as hp,
                            topTropical as trop, mangaTopOricon as mto, tagging,
                            twitter_search as ts, aqi, issfw, mediawiki, schedule,
                            anime_livechart, itunes, airing, apod, hospital as rsku,
-                           diceSim as dice)
+                           diceSim as dice, album)
 
 
 def lookup_zodiac(month, day):
@@ -73,37 +72,7 @@ def lookup_chinese_zodiac(year):
 
 
 def lookup_album_price():
-    album = requests.get('http://vgmdb.net/db/albums-search.php?do=results&action=upcoming')
-    soup = BeautifulSoup(album.text, "html.parser")
-    title = soup.find_all('span', {'class': 'albumtitle', 'lang': 'en'})
-    date = soup.find_all('td', {'style': 'padding: 4px; font-size:9pt; text-align: right'})
-    result = ''
-    i = 0
-    this_month = str(datetime.now().month)
-    album_date = date[0].next['href'][33]
-    while i < len(title) and (this_month == album_date):
-        album_date = date[i].next['href'][33]
-        album_title = title[i].text
-        if ('original' and 'soundtrack' in album_title.lower()):
-            result += album_title + ' : '
-            prices = soup.find_all('a', {'title': album_title})
-            album_url = prices[0]['href']
-            detail = requests.get(album_url)
-            beautiful = BeautifulSoup(detail.text, 'html.parser')
-            arr = beautiful.find_all('td')
-            album_price = arr[16].text.split(' ')
-            if (album_price[0].split('.')[0].isdigit()):
-                url = 'https://www.mataf.net/id/currency/converter-'
-                kurs = requests.get(url + album_price[1] + '-IDR?m1=' + album_price[0])
-                beautiful = BeautifulSoup(kurs.text, 'html.parser')
-                one_hundred = beautiful.find_all('input')[1]['value']
-                one = float(one_hundred)/100
-                total = one * float(album_price[0])
-                result += str(int(total)) + ' IDR\n'
-            else:
-                result += 'Tidak Dijual\n'
-        i += 1
-    return result
+    return album.Concrete_Album.template_method()
 
 
 def generate_schedule(chat_id, date, time, desc):
---
+++ csuibot/utils/album.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import abc
+
+
+class Album(metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        self.BASE_URL = "http://vgmdb.net/db/albums-search.php?do=results&action=upcoming"
+
+    def template_method(self):
+        return self.lookup_album_price()
+
+    def lookup_album_price(self):
+        pass
+
+
+class Concrete_Album(Album):
+
+    def lookup_album_price(self):
+        album = requests.get(self.BASE_URL)
+        soup = BeautifulSoup(album.text, "html.parser")
+        title = soup.find_all('span', {'class': 'albumtitle', 'lang': 'en'})
+        date = soup.find_all('td', {'style': 'padding: 4px; font-size:9pt; text-align: right'})
+        result = ''
+        i = 0
+        this_month = str(datetime.now().month)
+        album_date = date[0].next['href'][33]
+        while i < len(title) and (this_month == album_date):
+            album_date = date[i].next['href'][33]
+            album_title = title[i].text
+            if ('original' and 'soundtrack' in album_title.lower()):
+                result += album_title + ' : '
+                prices = soup.find_all('a', {'title': album_title})
+                album_url = prices[0]['href']
+                detail = requests.get(album_url)
+                beautiful = BeautifulSoup(detail.text, 'html.parser')
+                arr = beautiful.find_all('td')
+                album_price = arr[16].text.split(' ')
+                if (album_price[0].split('.')[0].isdigit()):
+                    url = 'https://www.mataf.net/id/currency/converter-'
+                    kurs = requests.get(url + album_price[1] + '-IDR?m1=' + album_price[0])
+                    beautiful = BeautifulSoup(kurs.text, 'html.parser')
+                    one_hundred = beautiful.find_all('input')[1]['value']
+                    one = float(one_hundred)/100
+                    total = one * float(album_price[0])
+                    result += str(int(total)) + ' IDR\n'
+                else:
+                    result += 'Tidak Dijual\n'
+            i += 1
+        return result
