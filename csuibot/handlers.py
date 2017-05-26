763732014c6ee6ccde5bc26ec421d2f0f38cf8f2
Felicia <feliciakrismanta@hotmail.com>
2017-05-26 14:20:47 +0700

    Added bugfix (#87)

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -1713,10 +1713,6 @@ def get_user_location_weather(message):
     chat_id = message.chat.id
     lat = message.location.latitude
     lon = message.location.longitude
-    app.logger.debug("lat")
-    app.logger.debug(lat)
-    app.logger.debug("lon")
-    app.logger.debug(lon)
     weather_result = lookup_weather(lat, lon, WIND_UNIT, TEMP_UNIT)
     markup = types.ReplyKeyboardRemove(selective=False)
     bot.send_message(chat_id, weather_result, markup)
