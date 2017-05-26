1d4630900392cbfcba9f7c7525a60a7aa3dbb288
Luthfi Aulia Sulaiman <luthfias@yahoo.com>
2017-05-26 17:50:22 +0700

    Ganti append ke dalam open w

--- csuibot/utils/acronym.py
+++ csuibot/utils/acronym.py
@@ -7,8 +7,8 @@ import json
 class Acronym:
 
     def add_acronym(self, message):
-        file = open('/app/csuibot/utils/acronym.json', 'r')
-        data = json.load(file)
+        with open('/app/csuibot/utils/acronym.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
         acronym_dict = message
         if acronym_dict['singkatan'] in data:
             message = "Data is already in library!"
@@ -16,11 +16,10 @@ class Acronym:
             print(acronym_dict['singkatan'])
             print(acronym_dict['acronym'])
             print("Enter else in add_acronym --> Acronym")
-            data.append(acronym_dict['singkatan'])
-            data[acronym_dict['singkatan']].append({"singkatan": acronym_dict['singkatan']})
-            data[acronym_dict['singkatan']].append({"acronym": acronym_dict['acronym']})
-            with open("/app/csuibot/utils/acronym.json", 'w') as file:
-                data = json.dump(data, file)
+            with open("/app/csuibot/utils/acronym.json", 'w', encoding='utf-8') as file:
+                entry = {acronym_dict['singkatan']: {"singkatan": acronym_dict['singkatan'], "acronym": acronym_dict['acronym']}}
+                data.append(entry)
+                json.dump(data, file)
 
             message = "{} - {} has been added!" \
                       .format(acronym_dict['singkatan'], acronym_dict['singkatan'])
