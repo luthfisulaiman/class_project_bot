9276a525906af9acc8673949214eede35fcdaa28
Luthfi Aulia Sulaiman <luthfias@yahoo.com>
2017-05-26 17:31:49 +0700

    Testing debug

--- csuibot/handlers.py
+++ csuibot/handlers.py
@@ -1031,6 +1031,7 @@ def process_add_acronym_step(message):
     print("Adding acronym")
     try:
         add__acronym = acronym_new(acronym_dict)
+        print(add_acronym)
     except ValueError:
         bot.reply_to(message, 'Error found!')
     else:
